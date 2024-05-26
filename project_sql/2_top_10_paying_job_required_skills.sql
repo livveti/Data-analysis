b20e8de84ba8386e93b55a70af89dc1f48ba8cb1
Wei Ting Liu(William Liu) <adrinduta@gmail.com>
2024-05-26 21:20:16 +0700

    Adds readme and images

--- project_sql/2_top_10_paying_job_required_skills.sql
+++ project_sql/2_top_10_paying_job_required_skills.sql
@@ -3,10 +3,8 @@ SELECT
   job_id,
   job_title,
   name AS company_name,
---  job_location,
   job_schedule_type,
   salary_year_avg
---  job_posted_date::DATE
 FROM job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 WHERE
